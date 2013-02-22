316f33e29caee5515d74530510618944073e7af2
Shannon <magsol@gmail.com>
2013-02-22 09:30:25 -0500

    Fixed a bunch of whitespace inconsistencies, generally tidied things up.

--- core/src/main/java/org/apache/mahout/clustering/spectral/kmeans/SpectralKMeansDriver.java
+++ core/src/main/java/org/apache/mahout/clustering/spectral/kmeans/SpectralKMeansDriver.java
@@ -17,10 +17,12 @@
 
 package org.apache.mahout.clustering.spectral.kmeans;
 
-import org.apache.commons.cli2.builder.DefaultOptionBuilder;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.mahout.clustering.Cluster;
 import org.apache.mahout.clustering.kmeans.KMeansDriver;
@@ -31,22 +33,15 @@ import org.apache.mahout.clustering.spectral.common.UnitVectorizerJob;
 import org.apache.mahout.clustering.spectral.common.VectorMatrixMultiplicationJob;
 import org.apache.mahout.common.AbstractJob;
 import org.apache.mahout.common.ClassUtils;
+import org.apache.mahout.common.HadoopUtil;
 import org.apache.mahout.common.commandline.DefaultOptionCreator;
 import org.apache.mahout.common.distance.DistanceMeasure;
-import org.apache.mahout.common.HadoopUtil;
-import org.apache.mahout.common.iterator.sequencefile.SequenceFileIterable;
-import org.apache.mahout.common.Pair;
 import org.apache.mahout.math.Vector;
 import org.apache.mahout.math.decomposer.lanczos.LanczosState;
 import org.apache.mahout.math.hadoop.DistributedRowMatrix;
 import org.apache.mahout.math.hadoop.decomposer.DistributedLanczosSolver;
 import org.apache.mahout.math.hadoop.decomposer.EigenVerificationJob;
-import org.apache.mahout.math.hadoop.stochasticsvd.SSVDSolver; 
-import org.apache.mahout.math.VectorWritable;
-
-import java.io.*;
-import java.util.List;
-import java.util.Map;
+import org.apache.mahout.math.hadoop.stochasticsvd.SSVDSolver;
 
 
 /**
@@ -66,60 +61,52 @@ import java.util.Map;
  */
 public class SpectralKMeansDriver extends AbstractJob {
 
-  public static final double OVERSHOOTMULTIPLIER = 2.0;
+	public static final double OVERSHOOTMULTIPLIER = 2.0;
 
-  public static void main(String[] args) throws Exception {
-    ToolRunner.run(new SpectralKMeansDriver(), args);
-  }
+	public static void main(String[] args) throws Exception {
+		ToolRunner.run(new SpectralKMeansDriver(), args);
+	}
   
-  @Override
-  public int run(String[] arg0)
-    throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, InterruptedException {
+	@Override
+	public int run(String[] arg0)
+			throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, InterruptedException {
     
-    Configuration conf = getConf();
-    addInputOption();
-    addOutputOption();
-    addOption("dimensions", "d", "Square dimensions of affinity matrix", true);
-    addOption("clusters", "k", "Number of clusters and top eigenvectors", true);
-    addOption(DefaultOptionCreator.distanceMeasureOption().create());
-    addOption(DefaultOptionCreator.convergenceOption().create());
-    addOption(DefaultOptionCreator.maxIterationsOption().create());
-    addOption(DefaultOptionCreator.overwriteOption().create());
-    addFlag("deletecalc", "f","Will delete calculation data on filesystem upon completion. Default is to keep these files.");
-    addFlag("ssvd", "e","Uses SSVD Eigensolver. Default is Lanczos solver.");
-    
-    Map<String, List<String>> parsedArgs = parseArguments(arg0);
-    if (parsedArgs == null) {
-      return 0;
-    }
-
-    Path input = getInputPath();
-    Path output = getOutputPath();
-    if (hasOption(DefaultOptionCreator.OVERWRITE_OPTION)) {
-    	HadoopUtil.delete(conf, output); }
-    int numDims = Integer.parseInt(getOption("dimensions"));
-    int clusters = Integer.parseInt(getOption("clusters"));
-    String measureClass = getOption(DefaultOptionCreator.DISTANCE_MEASURE_OPTION);
-    DistanceMeasure measure = ClassUtils.instantiateAs(measureClass, DistanceMeasure.class);
-    double convergenceDelta = Double.parseDouble(getOption(DefaultOptionCreator.CONVERGENCE_DELTA_OPTION));
-    int maxIterations = Integer.parseInt(getOption(DefaultOptionCreator.MAX_ITERATIONS_OPTION));
-    
-    boolean keepCalcFiles = true;
-    boolean ssvd = false;
-    
-    if(hasOption("deletecalc"))
-    {
-    	keepCalcFiles = false;
-    }
-    if(hasOption("ssvd"))
-    {
-    	ssvd = true;
-    }
-
-    run(conf, input, output, numDims, clusters, measure, convergenceDelta, maxIterations, keepCalcFiles, ssvd);
-
-    return 0;
-  }
+		Configuration conf = getConf();
+		addInputOption();
+		addOutputOption();
+		addOption("dimensions", "d", "Square dimensions of affinity matrix", true);
+		addOption("clusters", "k", "Number of clusters and top eigenvectors", true);
+		addOption(DefaultOptionCreator.distanceMeasureOption().create());
+		addOption(DefaultOptionCreator.convergenceOption().create());
+		addOption(DefaultOptionCreator.maxIterationsOption().create());
+		addOption(DefaultOptionCreator.overwriteOption().create());
+		addFlag("deletecalc", "f","Will delete calculation data on filesystem upon completion. Default is to keep these files.");
+		addFlag("ssvd", "e","Uses SSVD Eigensolver. Default is Lanczos solver.");
+		
+		Map<String, List<String>> parsedArgs = parseArguments(arg0);
+		if (parsedArgs == null) {
+		  return 0;
+		}
+		
+		Path input = getInputPath();
+		Path output = getOutputPath();
+		if (hasOption(DefaultOptionCreator.OVERWRITE_OPTION)) {
+			HadoopUtil.delete(conf, output);
+		}
+		int numDims = Integer.parseInt(getOption("dimensions"));
+		int clusters = Integer.parseInt(getOption("clusters"));
+		String measureClass = getOption(DefaultOptionCreator.DISTANCE_MEASURE_OPTION);
+		DistanceMeasure measure = ClassUtils.instantiateAs(measureClass, DistanceMeasure.class);
+		double convergenceDelta = Double.parseDouble(getOption(DefaultOptionCreator.CONVERGENCE_DELTA_OPTION));
+		int maxIterations = Integer.parseInt(getOption(DefaultOptionCreator.MAX_ITERATIONS_OPTION));
+		
+		boolean keepCalcFiles = (hasOption("deletecalc") ? false : true);
+		boolean ssvd = (hasOption("ssvd") ? true : false);
+		
+		run(conf, input, output, numDims, clusters, measure, convergenceDelta, maxIterations, keepCalcFiles, ssvd);
+		
+		return 0;
+	}
 
   /**
    * Run the Spectral KMeans clustering on the supplied arguments
@@ -133,140 +120,129 @@ public class SpectralKMeansDriver extends AbstractJob {
    * @param convergenceDelta the double convergence delta for the k-Means calculations
    * @param maxIterations the int maximum number of iterations for the k-Means calculations
    */
-  public static void run(
+	public static void run(
 		  Configuration conf,
-          Path input,
-          Path output,
-          int numDims,
-          int clusters,
-          DistanceMeasure measure,
-          double convergenceDelta,
-          int maxIterations,
-          boolean keepCalcFiles,
-          boolean ssvd)
-    throws IOException, InterruptedException, ClassNotFoundException {
+		  Path input,
+		  Path output,
+		  int numDims,
+		  int clusters,
+		  DistanceMeasure measure,
+		  double convergenceDelta,
+		  int maxIterations,
+		  boolean keepCalcFiles,
+		  boolean ssvd)
+				  throws IOException, InterruptedException, ClassNotFoundException {
     
-    Path outputCalc = new Path(output, "calculations");
-    Path outputTmp = new Path(output, "temporary");
+		Path outputCalc = new Path(output, "calculations");
+		Path outputTmp = new Path(output, "temporary");
 
-    // Take in the raw CSV text file and split it ourselves,
-    // creating our own SequenceFiles for the matrices to read later 
-    // (similar to the style of syntheticcontrol.canopy.InputMapper)
-    Path affSeqFiles = new Path(outputCalc, "seqfile");
-    AffinityMatrixInputJob.runJob(input, affSeqFiles, numDims, numDims);
-
-    // Construct the affinity matrix using the newly-created sequence files
-    DistributedRowMatrix A = 
-    		new DistributedRowMatrix(affSeqFiles, new Path(outputTmp, "afftmp"), numDims, numDims); 
-
-    Configuration depConf = new Configuration(conf);
-    A.setConf(depConf);
-    
-    // Construct the diagonal matrix D (represented as a vector)
-    Vector D = MatrixDiagonalizeJob.runJob(affSeqFiles, numDims);
-    
-    //Calculate the normalized Laplacian of the form: L = D^(-0.5)AD^(-0.5)
-    DistributedRowMatrix L = VectorMatrixMultiplicationJob.runJob(affSeqFiles, D,
-    		new Path(outputCalc, "laplacian"), new Path(outputCalc, outputCalc));
-   	L.setConf(depConf);
-    
-    Path data;
-
-   if(ssvd)
-   	{ // Otherwise uses slower Lanczos   
-	// SSVD requires an array of Paths to function. So we pass in an array of length one
-    Path[] LPath = new Path[1];
-   	LPath[0] = L.getRowPath();
-   	
-   	Path SSVDout = new Path(outputCalc, "SSVD");
-   	
-   	SSVDSolver solveIt = 
-   	new SSVDSolver(	depConf, 
-   					LPath, 
-   					SSVDout, 
-   					1000, // Vertical height of a q-block
-   					clusters, 
-   					15, // Oversampling 
-   					100); // # of reduce tasks
-   	
-  	solveIt.setComputeV(false); 
-  	solveIt.setComputeU(true);
-  	solveIt.setOverwrite(true);
-  	solveIt.setQ(0); // Set the power iterations (0 was fastest most accurate in testing)
-  	solveIt.setBroadcast(false); 
-  	// setBroadcast should be set to true is running on a distributed system, but on a single
-  	// machine it must be set to false. The documentation says that the default is false but 
-  	// the default is actually true. 
-
-  	solveIt.run();
-  	data = new Path(solveIt.getUPath()); // Needs "new Path", getUPath method returns a String
-  	
-   }
-   else
-   {
-	    // Perform eigen-decomposition using LanczosSolver
-	    // since some of the eigen-output is spurious and will be eliminated
-	    // upon verification, we have to aim to overshoot and then discard
-	    // unnecessary vectors later
-	    int overshoot = (int) ((double) clusters * OVERSHOOTMULTIPLIER);
-	    DistributedLanczosSolver solver = new DistributedLanczosSolver();
-	    LanczosState state = new LanczosState(L, overshoot, solver.getInitialVector(L));
-	    Path lanczosSeqFiles = new Path(outputCalc, "eigenvectors");
-	    
-	    solver.runJob(conf,
-	                  state,
-	                  overshoot,
-	                  true,
-	                  lanczosSeqFiles.toString());
-
-	    // perform a verification
-	    EigenVerificationJob verifier = new EigenVerificationJob();
-	    Path verifiedEigensPath = new Path(outputCalc, "eigenverifier");
-	    verifier.runJob(conf, 
-	    				lanczosSeqFiles, 
-	    				L.getRowPath(), 
-	    				verifiedEigensPath, 
-	    				true, 
-	    				1.0, 
-	    				clusters);
-
-	    Path cleanedEigens = verifier.getCleanedEigensPath();
-	    DistributedRowMatrix W = new DistributedRowMatrix(cleanedEigens, new Path(cleanedEigens, "tmp"), clusters, numDims);
-	    W.setConf(depConf);
-	    DistributedRowMatrix Wtrans = W.transpose();
-	    data = Wtrans.getRowPath();
-   }
-   	
-
-   		// Normalize the rows of Wt to unit length
-   		// normalize is important because it reduces the occurrence of two unique clusters  combining into one 
-	   Path unitVectors = new Path(outputCalc, "unitvectors");
-
-	   UnitVectorizerJob.runJob(data, unitVectors);
-
-	   DistributedRowMatrix Wt = new DistributedRowMatrix(unitVectors, new Path(unitVectors, "tmp"), clusters, numDims);
-	   Wt.setConf(depConf);
-	   data = Wt.getRowPath();
-
-
-    
-    // Generate random initial clusters
-    Path initialclusters = RandomSeedGenerator.buildRandom(conf, data,
-    		new Path(output, Cluster.INITIAL_CLUSTERS_DIR), clusters, measure);
-   
-    // Run the KMeansDriver
-    Path answer = new Path(output, "kmeans_out");
-	KMeansDriver.run(conf, data, initialclusters, answer,
-	    		measure,convergenceDelta, maxIterations, true, 0.0, false);
- 
-	
-	if(!keepCalcFiles){
-		HadoopUtil.delete(conf, outputCalc);
-	}
-    
-  	}
-  }
-
-
-
-////////////////////////  End of File  //////////////////////////////////////////////////
+		// Take in the raw CSV text file and split it ourselves,
+		// creating our own SequenceFiles for the matrices to read later 
+		// (similar to the style of syntheticcontrol.canopy.InputMapper)
+		Path affSeqFiles = new Path(outputCalc, "seqfile");
+		AffinityMatrixInputJob.runJob(input, affSeqFiles, numDims, numDims);
+		
+		// Construct the affinity matrix using the newly-created sequence files
+		DistributedRowMatrix A = 
+				new DistributedRowMatrix(affSeqFiles, new Path(outputTmp, "afftmp"), numDims, numDims); 
+		
+		Configuration depConf = new Configuration(conf);
+		A.setConf(depConf);
+		
+		// Construct the diagonal matrix D (represented as a vector)
+		Vector D = MatrixDiagonalizeJob.runJob(affSeqFiles, numDims);
+		
+		//Calculate the normalized Laplacian of the form: L = D^(-0.5)AD^(-0.5)
+		DistributedRowMatrix L = VectorMatrixMultiplicationJob.runJob(affSeqFiles, D,
+				new Path(outputCalc, "laplacian"), new Path(outputCalc, outputCalc));
+		L.setConf(depConf);
+		
+		Path data;
+		
+		if (ssvd) { // Otherwise uses slower Lanczos   
+			// SSVD requires an array of Paths to function. So we pass in an array of length one
+			Path [] LPath = new Path[1];
+			LPath[0] = L.getRowPath();
+			
+			Path SSVDout = new Path(outputCalc, "SSVD");
+			
+			SSVDSolver solveIt = new SSVDSolver(
+					depConf, 
+					LPath, 
+					SSVDout, 
+					1000, // Vertical height of a q-block
+					clusters, 
+					15, // Oversampling 
+					100); // # of reduce tasks
+			
+			solveIt.setComputeV(false); 
+			solveIt.setComputeU(true);
+			solveIt.setOverwrite(true);
+			solveIt.setQ(0); // Set the power iterations (0 was fastest most accurate in testing)
+			solveIt.setBroadcast(false); 
+			// setBroadcast should be set to true is running on a distributed system, but on a single
+			// machine it must be set to false. The documentation says that the default is false but 
+			// the default is actually true. 
+			
+			solveIt.run();
+			data = new Path(solveIt.getUPath()); // Needs "new Path", getUPath method returns a String
+		} else {
+			// Perform eigen-decomposition using LanczosSolver
+			// since some of the eigen-output is spurious and will be eliminated
+			// upon verification, we have to aim to overshoot and then discard
+			// unnecessary vectors later
+			int overshoot = (int) ((double) clusters * OVERSHOOTMULTIPLIER);
+			DistributedLanczosSolver solver = new DistributedLanczosSolver();
+			LanczosState state = new LanczosState(L, overshoot, solver.getInitialVector(L));
+			Path lanczosSeqFiles = new Path(outputCalc, "eigenvectors");
+			
+			solver.runJob(conf,
+			              state,
+			              overshoot,
+			              true,
+			              lanczosSeqFiles.toString());
+			
+			// perform a verification
+			EigenVerificationJob verifier = new EigenVerificationJob();
+			Path verifiedEigensPath = new Path(outputCalc, "eigenverifier");
+			verifier.runJob(conf, 
+							lanczosSeqFiles, 
+							L.getRowPath(), 
+							verifiedEigensPath, 
+							true, 
+							1.0, 
+							clusters);
+			
+			Path cleanedEigens = verifier.getCleanedEigensPath();
+			DistributedRowMatrix W = new DistributedRowMatrix(
+					cleanedEigens, new Path(cleanedEigens, "tmp"), clusters, numDims);
+			W.setConf(depConf);
+			DistributedRowMatrix Wtrans = W.transpose();
+			data = Wtrans.getRowPath();
+		}
+		
+		// Normalize the rows of Wt to unit length
+		// normalize is important because it reduces the occurrence of two unique clusters  combining into one 
+		Path unitVectors = new Path(outputCalc, "unitvectors");
+		
+		UnitVectorizerJob.runJob(data, unitVectors);
+		
+		DistributedRowMatrix Wt = new DistributedRowMatrix(
+				unitVectors, new Path(unitVectors, "tmp"), clusters, numDims);
+		Wt.setConf(depConf);
+		data = Wt.getRowPath();
+		
+		// Generate random initial clusters
+		Path initialclusters = RandomSeedGenerator.buildRandom(conf, data,
+				new Path(output, Cluster.INITIAL_CLUSTERS_DIR), clusters, measure);
+		   
+		    // Run the KMeansDriver
+		Path answer = new Path(output, "kmeans_out");
+		KMeansDriver.run(conf, data, initialclusters, answer,
+				measure,convergenceDelta, maxIterations, true, 0.0, false);
+		 
+		if(!keepCalcFiles){
+			HadoopUtil.delete(conf, outputCalc);
+		}
+    }
+}
